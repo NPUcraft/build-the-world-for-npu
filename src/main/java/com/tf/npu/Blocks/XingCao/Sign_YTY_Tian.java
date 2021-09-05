5cabf9350e4b178f25a5d9fd5d7f929f622a6195
1220891583 <48829109+1220891583@users.noreply.github.com>
2021-09-06 01:39:56 +0800

    设置分组

--- src/main/java/com/tf/npu/Blocks/XingCao/Sign_YTY_Tian.java
+++ src/main/java/com/tf/npu/Blocks/XingCao/Sign_YTY_Tian.java
@@ -2,6 +2,7 @@ package com.tf.npu.Blocks.XingCao;
 
 import com.tf.npu.Init.ModBlocks;
 import com.tf.npu.Init.ModItems;
+import com.tf.npu.NPU;
 import com.tf.npu.Template.BlockFourSideFacingTemplate;
 import net.minecraft.item.ItemBlock;
 
@@ -10,6 +11,7 @@ public class Sign_YTY_Tian extends BlockFourSideFacingTemplate {
         super();
         this.setUnlocalizedName("sign_yty_tian");
         this.setRegistryName("npu:sign_yty_tian");
+        this.setCreativeTab(NPU.MY_TAB2);
         ModItems.ITEMS.add(new ItemBlock(this).setRegistryName(this.getRegistryName()));
         ModBlocks.BLOCKS.add(this);
     }
