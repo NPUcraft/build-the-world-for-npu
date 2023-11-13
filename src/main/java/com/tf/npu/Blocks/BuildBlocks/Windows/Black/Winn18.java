d61fbee83f05f071f4ff29f3a69b65b957aa656f
Lozumi Everkine <fantasynico1026@gmail.com>
2023-11-14 02:15:50 +0800

    feat(Winn18&Winn19): Add active handler for Winn18&Winn19

--- src/main/java/com/tf/npu/Blocks/BuildBlocks/Windows/Black/Winn18.java
+++ src/main/java/com/tf/npu/Blocks/BuildBlocks/Windows/Black/Winn18.java
@@ -11,12 +11,11 @@ import net.minecraft.block.properties.PropertyDirection;
 import net.minecraft.block.state.BlockStateContainer;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemBlock;
 import net.minecraft.item.ItemStack;
-import net.minecraft.util.BlockRenderLayer;
-import net.minecraft.util.EnumFacing;
-import net.minecraft.util.IStringSerializable;
+import net.minecraft.util.*;
 import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockAccess;
@@ -50,13 +49,26 @@ public class Winn18 extends Block implements IHasModel {
     }
 
     //x1,y1,z1,x2,y2,z2均为double类型
-    public static final AxisAlignedBB Winn10_AABB = new AxisAlignedBB(0.0D, 0.0D, 0.0D, 1.0D, 1.0D, 1.0D);
+    public static final AxisAlignedBB WEST_AABB = new AxisAlignedBB(0.0D, -1.0D, 0.0D, 1.0D, 2.0D, 2.0D);
+    public static final AxisAlignedBB EAST_AABB = new AxisAlignedBB(0.0D, -1.0D, -1.0D, 1.0D, 2.0D, 1.0D);
+    public static final AxisAlignedBB SOUTH_AABB = new AxisAlignedBB(0.0D, -1.0D, 0.0D, 2.0D, 2.0D, 1.0D);
+    public static final AxisAlignedBB NORTH_AABB = new AxisAlignedBB(-1.0D, -1.0D, 0.0D, 1.0D, 2.0D, 1.0D);
 
     @Override
     public AxisAlignedBB getBoundingBox(IBlockState state, IBlockAccess source, BlockPos pos) {
-        return Winn10_AABB;
+        EnumFacing currentFacing = EnumFacing.byHorizontalIndex(getMetaFromState(state) & 3);
+        switch (currentFacing) {
+            case EAST:
+                return EAST_AABB;
+            case WEST:
+                return WEST_AABB;
+            case SOUTH:
+                return SOUTH_AABB;
+            case NORTH:
+                return NORTH_AABB;
+        }
+        return WEST_AABB;
     }
-
     @Override
     public boolean isOpaqueCube(IBlockState state) {
         return false;
@@ -113,4 +125,18 @@ public class Winn18 extends Block implements IHasModel {
         return true;
     }
 
+    @Override
+    public boolean onBlockActivated(World worldIn, BlockPos pos, IBlockState state, EntityPlayer playerIn, EnumHand hand, EnumFacing facing, float hitX, float hitY, float hitZ) {
+        // 获取原朝向
+        IBlockState newState = Block.getBlockFromName("npu:winn19").getDefaultState();
+        // 设置新朝向
+        EnumFacing currentFacing = EnumFacing.byHorizontalIndex(getMetaFromState(state) & 3);
+        newState = newState.withProperty(FACING, currentFacing);
+        worldIn.setBlockState(pos, newState);
+        // Post placement event
+        worldIn.notifyBlockUpdate(pos, state, newState, 3);
+        // Notify neighbors
+        worldIn.notifyNeighborsOfStateChange(pos, this, false);
+        return true;
+    }
 }
